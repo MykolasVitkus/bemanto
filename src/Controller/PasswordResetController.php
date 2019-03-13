7358d1093853df6ceb22c370edc839d56b5baa58
Martynas Kasparavičius <martis.kasparavicius@gmail.com>
2019-03-13 14:08:16 +0200

    Created Symfony Form

--- src/Controller/PasswordResetController.php
+++ src/Controller/PasswordResetController.php
@@ -4,16 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\PasswordResetType;
 
 class PasswordResetController extends AbstractController
 {
     /**
      * @Route("/password_reset", name="password_reset")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $form = $this->createForm(PasswordResetType::class, [
+            'action' => $this->generateUrl('password_reset')
+        ]);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+        }
+
         return $this->render('password_reset/index.html.twig', [
             'pageTitle' => 'Password reset',
+            'email_form' => $form->createView(),
         ]);
     }
 }
