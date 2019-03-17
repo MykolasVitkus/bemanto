6d3ca462cf79c6f0968ac32186b8baa2b6e68cce
RenatasB <renatas.birstonas@gmail.com>
2019-03-17 23:40:06 +0200

    Added access control, added password constrains.

--- src/Form/PasswordChangeType.php
+++ src/Form/PasswordChangeType.php
@@ -18,15 +18,28 @@ class PasswordChangeType extends AbstractType
     {
         $builder
             ->add('currentpw', PasswordType::class, [
-                'attr'=> array('class'=>'form-control')
+                'attr'=> array('class'=>'form-control'),
+                'required' => true,
+                'label' => 'Current password',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
             ])
             ->add('newpw', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'The password fields must match.',
                 'options' => ['attr' => ['class' => 'form-control']],
                 'required' => true,
-                'first_options'  => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat Password'],
+                'first_options'  => ['label' => 'New password'],
+                'second_options' => ['label' => 'Repeat new password'],
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
