4408f1efdfdae68bfd8cd3b7d65b842e7f143c70
pockeviciuspovilas <pockeviciuspovilas@gmail.com>
2019-03-18 15:09:35 +0200

    Fixed routes

--- src/Controller/UserDeletionController.php
+++ src/Controller/UserDeletionController.php
@@ -15,31 +15,31 @@ class UserDeletionController extends AbstractController
     /**
      * @Route("/admin/deletion", name="user_deletion")
      */
-  
- 
+
+
     public function index(): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
         $repository = $this->getDoctrine()->getRepository(User::class);
         $user = $repository->findAll();
 
-        return $this->render('user_deletion/index.html.twig',[
-            'usersArray' => $userr,
+        return $this->render('user_deletion/index.html.twig', [
+            'usersArray' => $user,
         ]);
     }
 
 
+    /**
+     * @Route("/admin/deletion/{id}", name="admin_deleteuser")
+     */
 
     public function deleteUser($id)
 
     {
-    $em = $this->getDoctrine()->getManager();
-    $users = $em -> getRepository(User::class)->find($id);
-    $em -> remove($users);
-    $em -> flush();
-    return $this->redirectToroute('admin_deleteuser_def');
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository(User::class)->find($id);
+        $em->remove($users);
+        $em->flush();
+        return $this->redirectToroute('user_deletion');
     }
-
-
-
 }
