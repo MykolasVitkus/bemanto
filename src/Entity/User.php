847a47574c8aae2caca5bab065ec570dc2bf37ea
RenatasB <renatas.birstonas@gmail.com>
2019-04-27 14:15:00 +0300

    Add 'verified' field to User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -35,6 +35,11 @@ class User implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $verified;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -111,5 +116,17 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getVerified(): ?bool
+    {
+        return $this->verified;
+    }
+
+    public function setVerified(bool $verified): self
+    {
+        $this->Verified = $verified;
+
+        return $this;
     }
 }
