8096e49e6705540b9794a809d18ebb939f6d8954
Martynas Kasparavičius <martis.kasparavicius@gmail.com>
2019-05-17 21:11:37 +0300

    Event category can now be created. Shows a list of all categories'.

--- src/Controller/CategoryEditController.php
+++ src/Controller/CategoryEditController.php
@@ -4,19 +4,77 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\CategoryCreateType;
+use App\Entity\Category;
 
-/**
- * @Route("/admin", name="admin_cp")
- */
 class CategoryEditController extends AbstractController
 {
     /**
-     * @Route("/categories", name="category_edit")
+     * @Route("/admin/categories", name="categories")
      */
     public function index()
     {
+        $repository = $this->getDoctrine()->getRepository(Category::class);
+        $allCategories = $repository->findAll();
+        $categories = [];
+
+        foreach($allCategories as $category)
+        {
+            array_push($categories, $category);
+        }
+
         return $this->render('category_edit/index.html.twig', [
-            'controller_name' => 'CategoryEditController',
+            'pageTitle' => 'Kategorijos',
+            'categoriesArray' => $categories
+        ]);
+    }
+
+    /**
+     * @Route("/admin/category_create", name="category_create")
+     */
+    public function create(Request $request)
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryCreateType::class, [
+            'action' => $this->generateUrl('category_create')
+        ]);
+
+        $form->handleRequest($request);
+
+        $errorMessage = "Kategorija tokia pavadinimu jau yra sukurta!";
+        $errorType = "danger";
+        $errorTitle = null;
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $searchCategory = $this->getDoctrine()->getRepository(Category::class)->findOneBy([
+                'name' => $form->get('title')->getData()
+            ]);
+
+            if(isset($searchCategory))
+            {
+                $errorTitle = "Klaida!";
+            }
+            else
+            {
+                $category->setName($form->get('title')->getData());
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($category);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('categories');
+            }
+        }
+
+        return $this->render('category_edit/create.html.twig', [
+            'pageTitle' => 'Kategorijos kūrimas',
+            'errorTitle' => $errorTitle,
+            'errorMessage' => $errorMessage,
+            'errorType' => $errorType,
+            'create_form' => $form->createView()
         ]);
     }
 }
