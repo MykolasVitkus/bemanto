1317f1f86f254fdf3214a7db4a09db3c90466c4c
Mykolas <mykolasvitkus@gmail.com>
2019-05-18 12:33:02 +0300

    CreateAdminUserCommand and UserManager

--- src/Command/CreateAdminUserCommand.php
+++ src/Command/CreateAdminUserCommand.php
@@ -3,8 +3,53 @@
 
 namespace App\Command;
 
+use App\Entity\User;
+use App\Service\UserManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class CreateAdminUserCommand
+/*
+ * Command class used to create an admin user.
+ * For correct usage, use $ php bin/console app:create-user username password
+ */
+class CreateAdminUserCommand extends Command
 {
+    protected static $defaultName = 'app:create-user';
 
+    private $userManager;
+    public function __construct(UserManager $manager)
+    {
+        $this->userManager = $manager;
+        parent::__construct();
+    }
+    protected function configure()
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'The email of the user.')
+            ->addArgument('password', InputArgument::REQUIRED, 'The password of the user.')
+            ->setDescription('Creates a new user.')
+            ->setHelp('This command allows you to create a user with administrative roles.
+             To use correctly, type php bin/console app:create-user \'yourEmail\' \'yourPassword\' ')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('You are about to create an admin user');
+        $email = $input->getArgument('email');
+        if (!$this->userManager->isEmailTaken($email))
+        {
+            $password = $input->getArgument('password');
+            $this->userManager->newAdminUser($email, $password);
+            $output->writeln('=================================');
+            $output->writeln('User has been successfully created!');
+            $output->writeln('=================================');
+            die;
+        }
+        $output->writeln('=================================');
+        $output->writeln('This email is already taken!');
+        $output->writeln('=================================');
+    }
 }
--- src/Service/UserManager.php
+++ src/Service/UserManager.php
@@ -1,10 +1,45 @@
 <?php
 
 
-namespace App;
+namespace App\Service;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserManager
 {
+    private $passwordEncoder;
+    private $objectManager;
+    public function __construct(UserPasswordEncoderInterface $encoder, EntityManagerInterface $em)
+    {
+        $this->passwordEncoder = $encoder;
+        $this->objectManager = $em;
+    }
+
+    /**
+     * @param $email
+     * @param $password
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function newAdminUser($email, $password)
+    {
+        $user = new User();
+        $user->setEmail($email);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
+        $user->setRoles(['ROLE_ADMIN']);
+        $this->objectManager->persist($user);
+        $this->objectManager->flush();
+    }
+    public function isEmailTaken($email)
+    {
+        $repository = $this->objectManager->getRepository(User::class);
+        if ($repository->findOneBy(['email' => $email]))
+        {
+            return true;
+        }
+        else return false;
+    }
 
 }
